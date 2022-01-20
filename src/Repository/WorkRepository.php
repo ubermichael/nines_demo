b7e81963f6aff0515579aaf1ab0d41f2af9907ae
Michael Joyce <ubermichael@gmail.com>
2022-01-20 12:08:17 -0800

    ws

--- src/Repository/WorkRepository.php
+++ src/Repository/WorkRepository.php
@@ -2,6 +2,12 @@
 
 declare(strict_types=1);
 
+/*
+ * (c) 2022 Michael Joyce <mjoyce@sfu.ca>
+ * This source file is subject to the GPL v2, bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace App\Repository;
 
 use App\Entity\Work;
@@ -45,5 +51,4 @@ class WorkRepository extends ServiceEntityRepository {
 
         return $qb->getQuery();
     }
-
 }
